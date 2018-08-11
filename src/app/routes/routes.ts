e07cb2237296769cde1537e8a73cdfd24e4d032b
Glendon Small <gsmall@sonovate.com>
2018-08-11 17:06:03 +0100

    Add additional reason for joining fields to registration and integrated in API

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,8 +1,8 @@
 import { LayoutComponent } from '../layout/layout.component';
-
 import { SigninComponent } from './user/signin/signin.component';
 import { SignupComponent } from './user/signup/signup.component';
 import { LockComponent } from './user/lock/lock.component';
+import { AuthGuard } from '../_guards';
 import { RecoverComponent } from './user/recover/recover.component';
 import { Signinv2Component } from './user/signinv2/signinv2.component';
 import { Signupv2Component } from './user/signupv2/signupv2.component';
@@ -11,10 +11,10 @@ export const routes = [
 
     {
         path: '',
-        component: LayoutComponent,
+        component: LayoutComponent, canActivate: [AuthGuard],
         children: [
 
-            { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+           
             { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
             { path: 'kanban', loadChildren: './kanban/kanban.module#DashboardModule' },
             { path: 'widgets', loadChildren: './widgets/widgets.module#WidgetsModule' },
@@ -23,7 +23,8 @@ export const routes = [
             { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
             { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
             { path: 'maps', loadChildren: './maps/maps.module#MapsModule' },
-            { path: 'pages', loadChildren: './pages/pages.module#PagesModule' }
+            { path: 'pages', loadChildren: './pages/pages.module#PagesModule' },
+             { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
 
         ]
     },
@@ -38,6 +39,6 @@ export const routes = [
     { path: 'signupv2', component: Signupv2Component },
 
     // Not found
-    { path: '**', redirectTo: 'dashboard' }
+    { path: '**', redirectTo: 'signin' }
 
 ];
